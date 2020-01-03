0e0aeb3a45af8840c0d8e25d79e49135e6675d90
Mateo Durante <mdurante@cert.unlp.edu.ar>
2020-01-03 13:20:56 -0300

    multiple windows

--- run.py
+++ run.py
@@ -1,9 +1,52 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
-app = QApplication([])
-window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Hola Bar'))
-layout.addWidget(QPushButton('Chay BAr'))
-window.setLayout(layout)
-window.show()
-app.exec_()
+import sys
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton,
+                             QToolTip, QMessageBox, QLabel)
+
+
+class Window2(QMainWindow):                           # <===
+    def __init__(self, last_window):
+        super().__init__()
+        self.last_window = last_window
+        self.setWindowTitle("Second window")
+
+    def closeEvent(self, event):
+        event.accept()
+        self.last_window.show()
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "First Window"
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Start", self)
+        self.pushButton.move(275, 200)
+        self.pushButton.setToolTip("<h3>Start the Session</h3>")
+
+        self.pushButton.clicked.connect(self.window2)              # <===
+
+        self.main_window()
+
+    def main_window(self):
+        self.label = QLabel("Hey Bar!", self)
+        self.label.move(285, 175)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def window2(self):                                             # <===
+        self.w = Window2(self)
+        self.w.show()
+        self.hide()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec())
